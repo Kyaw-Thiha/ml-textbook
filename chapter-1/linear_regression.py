d00ae6683e88f84df8f2dfc460d84dbf71367715
Kevin_KyawThiha <k.thiha10.mail@gmail.com>
2025-09-04 12:56:46 -0400

    Added multi-dimensional section

--- chapter-1/linear_regression.py
+++ chapter-1/linear_regression.py
@@ -7,6 +7,7 @@ This is basically trying to find a line of best fit in a multi-dimenensional dat
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # |%%--%%| <YskWEeMKcA|WJdsVDiTKZ>
 r"""°°°
@@ -68,7 +69,7 @@ fig.add_trace(
         x=x_best,
         y=y_best,
         mode="lines",
-        name=f"Best fit: y={w:.3f}x+{b:.3f}",
+        name=f"y={w:.3f}x+{b:.3f}",
         line=dict(color="red"),
     )
 )
@@ -131,7 +132,7 @@ fig.add_trace(
         x=x1_best,
         y=x2_best,
         z=y_best,
-        name=f"fit: y={w_0:.2f}+{w_1:.2f}x1+{b:.2f}x2",
+        name=f"y={w_0:.2f}+{w_1:.2f}x1+{b:.2f}x2",
         opacity=0.6,
     )
 )
@@ -141,4 +142,80 @@ fig.update_layout(
 )
 fig.show()
 
-# |%%--%%| <3vKRjQy6EZ|zRSljPcpa9>
+# |%%--%%| <3vKRjQy6EZ|F3U5gizHkd>
+r"""°°°
+Multi-dimensional input & output
+°°°"""
+# |%%--%%| <F3U5gizHkd|8bVKZf4X57>
+
+input_dimension = 5
+output_dimension = 3
+points_num = 10
+low = 0
+high = 5
+x = np.random.uniform(low=low, high=high, size=(points_num, input_dimension))
+y = np.random.uniform(low=low, high=high, size=(points_num, output_dimension))
+print(f"x: {x}")
+print(f"y: {y}")
+
+new_column = np.ones((points_num, 1), dtype=int)
+x_tilde = np.hstack([x, new_column])
+print(f"x_tilde: {x_tilde}")
+
+x_transpose = np.transpose(x_tilde)
+x_pseudoinverse = np.linalg.inv((x_transpose @ x_tilde)) @ x_transpose
+w_tilde = x_pseudoinverse @ y
+print(f"w_tilde: {w_tilde}")
+print(f"Size (input_dimension + 1, output_dimension): {w_tilde.shape}")
+
+# |%%--%%| <8bVKZf4X57|CtlNYI16eB>
+
+fig = make_subplots(
+    rows=output_dimension,
+    cols=input_dimension,
+    subplot_titles=[
+        f"Input: {i % input_dimension}, Output: {i // input_dimension}"
+        for i in range(input_dimension * output_dimension)
+    ],
+)
+fig.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
+fig.update_layout(
+    height=600, width=1200, title="Linear Regression of Multi-dimensional data"
+)
+
+for output_v in range(output_dimension):
+    w_vec = w_tilde[:-1, output_v]
+    b0 = w_tilde[-1, output_v]
+    x_means = x.mean(axis=0)
+
+    for input_v in range(input_dimension):
+        x_coord = np.arange(low, high)
+        w = w_tilde[input_v, output_v]
+        # b = w_tilde[-1, output_v]
+        b = b0 + (w_vec @ x_means) - w_vec[input_v] * x_means[input_v]
+        y_best = w * x_coord + b
+        fig.add_trace(
+            go.Scatter(
+                x=x_coord,
+                y=y_best,
+                mode="lines",
+                name=f"y={w:.3f}x+{b:.3f}",
+                line=dict(color="red"),
+            ),
+            row=output_v + 1,
+            col=input_v + 1,
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=x[:, input_v],
+                y=y[:, output_v],
+                mode="markers",
+                name="Points",
+                marker=dict(size=10, color="blue"),
+            ),
+            row=output_v + 1,
+            col=input_v + 1,
+        )
+fig.show()
+
+# |%%--%%| <CtlNYI16eB|zRSljPcpa9>
